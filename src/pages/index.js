506efc49575f2b385bbafe28ade516c64c30c004
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-23 17:31:03 -0400

    Edit text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
                 className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl' />
               <p
                 className='my-4 text-base font-medium md:text-sm sm:text-xs'>
-                As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
-                Explore my latest projects, showcasing my expertise in React.js, Next.js, Node.js, Express.js, & Django and web development.
+                As a proficient full-stack developer, my focus is on translating concepts into innovative software applications. 
+                Explore my latest projects, showcasing my expertise in React.js, Reat Native, Next.js, Express.js, Node.js, & Django, for Web & Mobile development.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link
