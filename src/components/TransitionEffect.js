b6399a0a6d2a448adfaceac184554e8df52f3804
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-24 03:45:26 -0400

    Modify color.

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function TransitionEffect() {
     return (
         <>
-            <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
+            <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primaryDark'
                 initial={{ x: '100%', width: '100%' }}
                 animate={{ x: '0%', width: '0%' }}
                 exit={{ x: ['0%', '100%'], width: ['0%', '100%'] }}
