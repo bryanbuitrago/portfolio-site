14967d995e3450b2037ec7ea101c42e2e2dddc2c
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-24 03:45:03 -0400

    Modify Text Web & Mobile Version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -73,29 +73,21 @@ const NavBar = () => {
                 </nav>
                 <nav className='flex items-center justify-center flex-wrap'>
                     <motion.a
-                        href='https://twitter.com' target={'_blank'}
-                        whileHover={{ y: -2 }}
-                        whileTap={{ scale: 0.9 }}
-                        className='w-6 mr-3'
-                    >
-                        <TwitterIcon />
-                    </motion.a>
-                    <motion.a
-                        href='https://twitter.com'
+                        href='https://github.com/bryanbuitrago'
                         target={'_blank'}
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
                         className='w-6 mr-3'
-                    >
+                        >
                         <GithubIcon />
                     </motion.a>
                     <motion.a
-                        href='https://twitter.com'
+                        href='https://www.linkedin.com/in/bryan-buitrago-1229a3105/'
                         target={'_blank'}
                         whileHover={{ y: -2 }}
                         whileTap={{ scale: 0.9 }}
                         className='w-6 mr-3'
-                    >
+                        >
                         <LinkedInIcon />
                     </motion.a>
                     <button
@@ -128,32 +120,24 @@ const NavBar = () => {
                             <CustomMobileLink href='/projects' title='Projects' className='' toggle={handleClick} />
                         </nav>
                         <nav className='flex items-center justify-center flex-wrap mt-2'>
-                            <motion.a
-                                href='https://twitter.com' target={'_blank'}
-                                whileHover={{ y: -2 }}
-                                whileTap={{ scale: 0.9 }}
-                                className='w-6 mr-3 sm:mx-1'
-                            >
-                                <TwitterIcon />
-                            </motion.a>
-                            <motion.a
-                                href='https://twitter.com'
-                                target={'_blank'}
-                                whileHover={{ y: -2 }}
-                                whileTap={{ scale: 0.9 }}
-                                className='w-6 mr-3 bg-light rounded-full dark:bg-dark sm:mx-1'
-                            >
-                                <GithubIcon />
-                            </motion.a>
-                            <motion.a
-                                href='https://twitter.com'
-                                target={'_blank'}
-                                whileHover={{ y: -2 }}
-                                whileTap={{ scale: 0.9 }}
-                                className='w-6 mr-3 sm:mx-1'
-                            >
-                                <LinkedInIcon />
-                            </motion.a>
+                        <motion.a
+                        href='https://github.com/bryanbuitrago'
+                        target={'_blank'}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mr-3sm:mx-1'
+                        >
+                        <GithubIcon />
+                    </motion.a>
+                    <motion.a
+                        href='https://www.linkedin.com/in/bryan-buitrago-1229a3105/'
+                        target={'_blank'}
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='w-6 mr-3 sm:mx-1'
+                        >
+                        <LinkedInIcon />
+                    </motion.a>
                             <button
                                 onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
                                 className={`ml-3 flex items-center justify-items-center rounded-full p-1 
