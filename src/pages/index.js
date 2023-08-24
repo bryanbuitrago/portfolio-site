b0199b99b580d3b9aed5d99bd29981b31c47aae5
bryanbuitrago <bryanbuitrago86@gmail.com>
2023-08-24 15:21:21 -0400

    Add email address.

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
                   CV <LinkArrow className={'w-6 ml-1'} />
                 </Link>
                 <Link
-                  href='mailto:abcd@gmail.com'
+                  href='mailto:bryanbuitragomz@gmail.com'
                   target={'_blank'}
                   className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'>Contact</Link>
               </div>
